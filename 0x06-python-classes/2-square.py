4c4e88b0741cab9c76fcf6fb931c5d1c20095116
mathiyos <kebedemathiyos@gmail.com>
2022-06-21 15:58:27 +0300

    task two

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
-class Square:
-    """Type class square"""
-    def __init__(self, size=0):
-        """Init the square classs
-        Args:
-        param1: size is the type int attribute to make it private
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size#!/usr/bin/python3
-class Square:
-    """Type class square"""
-    def __init__(self, size=0):
-        """Init the square classs
-        Args:
-        param1: size is the type int attribute to make it private
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+"""Class Square"""
 
+
+class Square:
+
+    """Private instance attribute: size
+    size must be an integer
+    The size is greater than or equal to 0
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
